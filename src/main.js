f9a95fc72ac74d21358ca3580e61799b63bef0ac
Mészáros Róbert <hmm@meszarosrob.com>
2020-11-27 19:40:53 +0200

    Make the multiplier an object so we introduce more options

--- src/main.js
+++ src/main.js
@@ -118,7 +118,7 @@ const app = () => {
                     }
                     this.step = this.step + 1;
                     this.transitionToNextExercise();
-                }, secInMs(this.duration.exercise * this.exercise.multiplier));
+                }, secInMs(this.duration.exercise * this.exercise.multiplier.duration));
             }, secInMs(this.duration.between));
         },
         get exercise () {
